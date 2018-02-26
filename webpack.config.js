081583f7822865f2bec6d220adacf69b12189417
Michael Dunlap <michaeldunlap@earthlink.net>
2018-02-25 20:26:56 -0800

    fix two spelling errors in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const webpack = require('webpack')
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'developement'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 if (process.env.NODE_ENV === 'test') {
     require('dotenv').config({ path: '.env.test'})
@@ -46,7 +46,7 @@ module.exports = (env) => {
         },
         plugins: [
             CSSExtract,
-            new webpack.DefinePlugIn({
+            new webpack.DefinePlugin({
                 'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
                 'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
                 'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
